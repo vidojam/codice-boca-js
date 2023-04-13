2e2ea2fb7763c78d03fd2c05e1703cafeea7f8dd
Jose Torres <vidojam@gmail.com>
2023-04-13 13:13:22 -0400

    firebase run

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ function displayMenuItems(menuItems) {
     const div = document.createElement('div');
     const title = document.createElement('h2');
     const titleText = document.createTextNode(dish.title);
-    title.appendChild(titleText);
+    title.appendChild(titleText); // stck text into h2
     div.appendChild(title); // stick h2 into the div
     const description = document.createElement('p'); // create p
     const descriptionText = document.createTextNode(dish.description); //create the text for inside 
@@ -14,8 +14,8 @@ function displayMenuItems(menuItems) {
     div.appendChild(description);
     menuSection.appendChild(div);
 
-  })
-}
+  });
+};
 
 async function getMenuItems() {
   const response = await fetch('https://codice-boca.web.app/menu');
